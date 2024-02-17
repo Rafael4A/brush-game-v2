79afab48a4e68c253d86877bf644be9ef851cb2a
Rafael do Amaral P <rafael.do.amaralp@gmail.com>
2024-02-17 18:58:32 -0300

    Remove boilerplate code

--- TODO.md
+++ TODO.md
@@ -1,7 +1,5 @@
 ## Todo
 
-- Review SonarLint issues
 - Fix theme colors: filter only used
 - Add logging to API
 - Create releases to main
-- Fix deploy
--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,8 +3,6 @@ import { ConfigModule } from "@nestjs/config";
 import { ScheduleModule } from "@nestjs/schedule";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { CronModule } from "./cron/cron.module";
 import { dbdatasource } from "./data.source";
 import { GatewayModule } from "./gateway/gateway.module";
@@ -13,17 +11,11 @@ import { RoomModule } from "./room/room.module";
 @Module({
   imports: [
     ConfigModule.forRoot(),
-
     TypeOrmModule.forRoot(dbdatasource),
-
     GatewayModule,
-
     RoomModule,
-
     ScheduleModule.forRoot(),
     CronModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
